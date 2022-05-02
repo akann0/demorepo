0612b0c4215ff79cc9ddf8601fe5f6538e1d0522
akann0 <akann@ucsd.edu>
2022-05-01 20:18:48 -0700

    Made evertything

---
+++ demojava.java
@@ -0,0 +1,8 @@
+class demojava {
+    static String method(){
+        return Integer.toString(7);
+    }
+    public static void main(String[] args) {
+        System.out.println(method());
+    }
+}
---
+++ demotester.java
@@ -0,0 +1,11 @@
+import java.beans.Transient;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class demotester {
+    @Test 
+    public void test(){
+        assertEquals("7", demojava.method());
+    }
+}
